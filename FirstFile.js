4e2ca314d74e2bcb341ce7662e65c91e04c948a7
mkaef <eric_franck2@yahoo.fr>
2024-04-01 16:24:25 -0500

    Object and fuction as class

--- FirstFile.js
+++ FirstFile.js
@@ -1,11 +1,12 @@
 console.log("Client side scripting language, interpreted, oojs, functional, dynamic typing, case sensetive, auto casting")
 
-
-var user_Name ="Test user for node"
-
 PrintName(user_Name)
 
-function PrintName(name){
+var user_Name ="Test user for node" // undefined - gets hoisted with default value
+
+//PrintName(user_Name)
+
+function PrintName(name){ // gets hoisted (snapshot created)
     console.log(name)
 }
 

d1fd6df657ecdc7a056d0655eb3d816745804e39
mkaef <eric_franck2@yahoo.fr>
2024-05-08 19:04:07 -0500

    react

---
+++ ES6/react.js
@@ -0,0 +1,27 @@
+//React
+
+//React is a web library to create single page applications
+//Its light weight
+//has a very high performamce due to virtual dom
+//diffing algorithm
+//doesnt use html templates, instead craetes templates using jsx code
+//component based architecture
+
+//dom - document object model
+//dom-mutation or manipulation - upon interaction mutation happens and is very slow on browsers
+//creation of dom tree
+//element rendering
+//adjusting html elements 
+//css - style sheet applied
+//css - adjustments, font, spacing, 
+//color adjustments
+//final rendering
+
+// 60 frame/sec
+
+
+//create a copy of actual html in javascript object format - virtual dom
+//do the change in react component first, check there if its already present - new virtual dom
+
+//use diffing algorithm, to compare the new virtual dom with acutal dom, dfs
+//if change is there, just replace the node instead of re-rendering the html
